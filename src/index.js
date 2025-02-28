575bdcedc87318765ef11e6a8d984e0ffc812c74
Stex Coder <stex.coding@gmail.com>
2025-02-28 23:12:56 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const parallax = document.getElementsByClassName('parralax-effect')
+
+const parallax2 = document.getElementsByClassName('parralax-effect1')
+
+const parallax3 = document.getElementsByClassName('parralax-effect2')
+
